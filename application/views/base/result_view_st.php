03c4b6b3b4b79b455285712351a679b6f0e678bb
Orlando Hernandez <oaraz02@gmail.com>
2018-07-20 16:41:31 -0500

    change my objects

--- application/views/base/result_view_st.php
+++ application/views/base/result_view_st.php
@@ -5,54 +5,53 @@ if ($sess == 1) {
 ?>
 <link rel="stylesheet" href="<?php echo base_url()?>asset/raty/jquery.raty.css">
 </script><script src="<?php echo base_url()?>asset/raty/jquery.raty.js"></script>
-<section id="main-content" role="main">
-    <section class="wrapper">
 
-<div class="col-lg-12">
-    <!-- page start-->
-    <section class="panel">
-        <header class="panel-heading">
-            <a href="<?php echo base_url().$url?>"><button type="button" class="btn btn-success btn-sm"><li class="icon-reply"><b> Volver</b></li></button></a> <?php echo $encabezado?>: </b>
-            <div id="prueba"></div>
-        </header>
-        <div class="panel-body">
-            <?php
-            if (!empty($result)) {
-                foreach ($result as $key) {
-                    $url = base64_encode($key['lo_location']);
-                    $lo_name = base64_encode($key['lo_title']);
-                    ?>
-                    <div class="classic-search">
+<div class="content-inner">
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- page start-->
+                    <div class="card border-0">
+                        <h1 class="card-title"> <?php echo $encabezado?>:</h1>
+                        <div class="card-body">
+                            <?php
+                            if (!empty($result)) {
+                                foreach ($result as $key) {
+                                    $url = base64_encode($key['lo_location']);
+                                    $lo_name = base64_encode($key['lo_title']);
+                                    ?>
+                                    <div class="classic-search">
 
-                        <p><a target="_blank" class="titulo" id="<?php echo $key['lo_id'] ?>" rep_id="<?php echo $key['rep_id'] ?>" logged="<?php echo $logged ?>" href="<?php echo base_url().'lo/load_lo/'.$url.'/'.$lo_name ?>"><?php echo $key['lo_title'] ?></a>
-                        </p>
-                        <div>
-                            <b>Descripción: </b><?php echo $key['lo_description'] ?><br>
-                            <b>Palabras claves: </b><?php echo $key['lo_keyword'] ?><br>
+                                        <p><a target="_blank" class="titulo" id="<?php echo $key['lo_id'] ?>" rep_id="<?php echo $key['rep_id'] ?>" logged="<?php echo $logged ?>" href="<?php echo base_url().'lo/load_lo/'.$url.'/'.$lo_name ?>"><?php echo $key['lo_title'] ?></a>
+                                        </p>
+                                        <div>
+                                            <b>Descripción: </b><?php echo $key['lo_description'] ?><br>
+                                            <b>Palabras claves: </b><?php echo $key['lo_keyword'] ?><br>
 
-                            <a onclick="verMetadata('<?php echo $key['lo_id'] . '/' . $key['rep_id'] ?>')" class="btn btn-sm btn-info" data-toggle="modal" href="#dialog_medatada" txt="OA1">
-                                <li class="icon-eye-open"></li> Ver metadatos
-                            </a>
+                                            <a onclick="verMetadata('<?php echo $key['lo_id'] . '/' . $key['rep_id'] ?>')" class="btn btn-sm btn-info" data-toggle="modal" href="#dialog_medatada" txt="OA1">
+                                                <li class="icon-eye-open"></li> Ver metadatos
+                                            </a>
 
-                          <!--  <a onclick="verIndicadores('<?php echo $key['lo_id'] . '/' . $key['rep_id'] . '/' . $user; ?>','<?php echo $key['rank']?>')" class="btn btn-warning btn-sm" data-toggle="modal" href="#dialog_indicaores" txt="OA2">
-                                <li class="icon-eye-open"></li> Ver Indicadores
-                            </a>-->
+                                          <!--  <a onclick="verIndicadores('<?php echo $key['lo_id'] . '/' . $key['rep_id'] . '/' . $user; ?>','<?php echo $key['rank']?>')" class="btn btn-warning btn-sm" data-toggle="modal" href="#dialog_indicaores" txt="OA2">
+                                                <li class="icon-eye-open"></li> Ver Indicadores
+                                            </a>-->
+                                        </div>
+                                    </div>
+                                    <?php
+                                }
+                            } else {
+                                ?>
+                                No hay resultados.
+                            <?php } ?>
                         </div>
                     </div>
-                    <?php
-                }
-            } else {
-                ?>
-                No hay resultados.
-            <?php } ?>
-
-
+                    <!-- page end-->
+                </div>
+            </div>
         </div>
     </section>
-    <!-- page end-->
-</div>
-    </section>
-</section>
+
 <!-- Modal Metadata -->
 <div class="modal fade" id="dialog_medatada" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
