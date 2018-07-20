42ab80f018440efac35bc0349d28884364f5bf1a
Orlando Hernandez <oaraz02@gmail.com>
2018-07-20 16:22:46 -0500

    change profile view

--- application/views/usr/perfil_view.php
+++ application/views/usr/perfil_view.php
@@ -3,34 +3,36 @@
 <?php foreach($usr_all_data as $usr_data){ ?>
       <!--main content start-->
 
-      <section id="main-content" role="main">
-          <section class="wrapper">
-              <!-- page start-->
+      <div class="content-inner">
+          <section>
+          <div class="container-fluid">
               <div class="row">
-
                   <aside class="profile-nav col-lg-3" role="complementary" aria-label="Perfil del usuario">
-                      <section class="panel">
-                          <div class="user-heading round">
+                      <div class="card">
+                          <div class="card-header user-header">
                               <a href="#">
-                                  <img src="<?php echo base_url()?>asset/img/user.gif" alt="">
+                                  <img src="<?php echo base_url()?>asset/img/user.gif"  class="img-profile-user"alt="">
                               </a>
                               <h1><?php echo $usr_data['use_nombre'].' '.$usr_data['use_apellido']?></h1>
                               <p><?php echo $usr_data['use_email']?></p>
                           </div>
-
-                          <ul class="nlist-group list-group-flush">
-                              <li><a class="list-group-item" href="<?php echo base_url()?>usuario/mis_objetos"> <i class="icon-book"></i> Mis objetos</a></li>
-                             <!-- <li><a href="profile-activity.html"> <i class="icon-rss-sign"></i> Noticias <span class="label label-danger pull-right r-activity">9</span></a></li>-->
-                              <li><a class="list-group-item" href="<?php echo base_url()?>usuario/editar_usr"> <i class="icon-edit"></i> Editar perfil</a></li>
-                              <!--<li><a href="<?php echo base_url()?>usuario/estilos"> <i class="icon-edit"></i> Test de Estilos de Aprendizaje </a></li>>-->
-                          </ul>
-
-                      </section>
+                          <div class="card-body">
+                              <ul class="list-group list-group-flush profile-nav">
+                                  <li class="list-group-item"><a  href="<?php echo base_url()?>usuario/mis_objetos"> <i class="fa fa-book"></i> Mis objetos</a></li>
+                                 <!-- <li><a href="profile-activity.html"> <i class="icon-rss-sign"></i> Noticias <span class="label label-danger pull-right r-activity">9</span></a></li>-->
+                                  <li class="list-group-item"><a  href="<?php echo base_url()?>usuario/editar_usr"> <i class="fa fa-pencil"></i> Editar perfil</a></li>
+                                  <!--<li><a href="<?php echo base_url()?>usuario/estilos"> <i class="icon-edit"></i> Test de Estilos de Aprendizaje </a></li>>-->
+                              </ul>
+                          </div>
+                      </div>
                   </aside>
                   <aside class="profile-info col-lg-9" role="complementary" aria-label="Acerca del usuario">
-                      <section class="panel">
-                          <div class="panel-body bio-graph-info">
-                              <h1>Acerca de mi</h1>
+                      <div class="card border-0">
+
+                              <h2 class="card-title" style="color: #89817e;">Acerca de mi</h2>
+
+                          <div class="card-body bio-graph-info">
+
                               <div class="row">
                                   <div class="bio-row">
                                      <p><span>Nombres: </span> <?php echo $usr_data['use_nombre']?></p>
@@ -62,12 +64,10 @@
                                   </div>
                               </div>
                           </div>
-                      </section>
+                      </div>
                   </aside>
               </div>
-
-              <!-- page end-->
-          </section>
+          </div>
       </section>
 <?php };?>
       <!--main content end-->
