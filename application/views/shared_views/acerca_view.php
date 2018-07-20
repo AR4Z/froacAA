b8a5facade091b741a1be96c837cecee5c53aecf
Orlando Hernandez <oaraz02@gmail.com>
2018-07-20 12:00:08 -0500

    change about view and fix error in team view

--- application/views/shared_views/acerca_view.php
+++ application/views/shared_views/acerca_view.php
@@ -1,38 +1,19 @@
-<!--main content start-->
-<section id="main-content" role="main">
-    <section class="wrapper">
-
-        <section class="panel">
-            <header class="panel-heading">
-                 <h1>Acerca de</h1>
-            </header>
-            <div class="panel-body">
-                <div class="row">
-
-    </div>
-    <div class="art-postcontent"><br>
+<div class="content-inner" role="main">
+        <div class="container-fluid">
+            <br/>
+            <div class="card border-0">
+                    <h1>Acerca de FROAC</h1>
+                <div class="card-body">
+                    <p>FROAC es la sigla en español de la Federación de Repositorios de Objetos de Aprendizaje de Colombia, que surge como resultado del proyecto de investigación financiado por COLCIENCIAS titulado: “ROAC Creación de un modelo para la Federación de OA en Colombia que permita su integración a confederaciones internacionales”, de la Universidad Nacional de Colombia, con código 1119-521-29361.</p>
 
 
-
-        <p>FROAC es la sigla en español de la Federación de Repositorios de Objetos de Aprendizaje de Colombia, que surge como resultado del proyecto de investigación financiado por COLCIENCIAS titulado: “ROAC Creación de un modelo para la Federación de OA en Colombia que permita su integración a confederaciones internacionales”, de la Universidad Nacional de Colombia, con código 1119-521-29361.</p>
-
-        <h5 align="center">Resumen del Proyecto:</h5>
-        <p>
-        El soporte de TICs al aprendizaje puede beneficiar a gran cantidad de personas, facilitar la reutilización de materiales educativos y aplicación de estrategias de enseñanzas diversas. Parte importante de las investigaciones están orientadas al diseño y creación de Objetos de Aprendizaje OA y de los Repositorios que permiten su intercambio.
-        La estrategia de contenidos educacionales en pequeños entidades es un avance significativo en la producción de OA, pero esto por sí solo no es suficiente. Su real utilidad requiere que estos objetos sean compatibles con diversos ambientes y sistemas de administración de aprendizajes, fáciles de migrar de una plataforma a otra, fáciles de localizar, acceder, archivar y re-utilizar. De allí la importancia del establecimiento de estándares para el diseño y descripción de los OA. Los potenciales beneficios de los Repositorios de AO son la reutilización, interoperatividad, durabilidad y accesibilidad.
-        El proyecto pretende definir  y validar  cuáles son las características y tecnologías que posibilitan efectivamente el almacenamiento y recuperación de OA reutilizables y que puedan apuntar a participar  en Federaciones de Repositorios de OA tanto nacionales como internacionales.  Se propone desarrollar un modelo para la creación en Colombia de un conjunto de repositorios de OA federados que permitan compartir  estos recursos mediante la definición de políticas, bases metodológicas y procesos de construcción y administración de los OA y repositorios, con base en estándares internacionales y software libre.</p>
-
-
-</div>
-    <div class="cleared"></div>
-</div>
-
-        </section>
-
-
-        <div class="row"  id="result">
-
+                    <h5 align="center">Resumen del Proyecto:</h5>
+                    <p>
+                    El soporte de TICs al aprendizaje puede beneficiar a gran cantidad de personas, facilitar la reutilización de materiales educativos y aplicación de estrategias de enseñanzas diversas. Parte importante de las investigaciones están orientadas al diseño y creación de Objetos de Aprendizaje OA y de los Repositorios que permiten su intercambio.
+                    La estrategia de contenidos educacionales en pequeños entidades es un avance significativo en la producción de OA, pero esto por sí solo no es suficiente. Su real utilidad requiere que estos objetos sean compatibles con diversos ambientes y sistemas de administración de aprendizajes, fáciles de migrar de una plataforma a otra, fáciles de localizar, acceder, archivar y re-utilizar. De allí la importancia del establecimiento de estándares para el diseño y descripción de los OA. Los potenciales beneficios de los Repositorios de AO son la reutilización, interoperatividad, durabilidad y accesibilidad.
+                    El proyecto pretende definir  y validar  cuáles son las características y tecnologías que posibilitan efectivamente el almacenamiento y recuperación de OA reutilizables y que puedan apuntar a participar  en Federaciones de Repositorios de OA tanto nacionales como internacionales.  Se propone desarrollar un modelo para la creación en Colombia de un conjunto de repositorios de OA federados que permitan compartir  estos recursos mediante la definición de políticas, bases metodológicas y procesos de construcción y administración de los OA y repositorios, con base en estándares internacionales y software libre.</p>
+                </div>
+            </div>
         </div>
-    </section>
-</section>
+
 <!--main content end-->
